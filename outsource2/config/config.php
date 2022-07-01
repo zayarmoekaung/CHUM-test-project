6a8521169f9bbe84b4098370fc5722ea52835fd3
zayarmoekaung <42345198+zayarmoekaung@users.noreply.github.com>
2022-07-01 09:18:52 +0630

    Update config.php

--- outsource2/config/config.php
+++ outsource2/config/config.php
@@ -7,8 +7,3 @@ define('APP_ROOT', dirname(dirname(__FILE__)));
 define('URL_ROOT', '/');
 define('URL_SUBFOLDER', 'CHUM/outsource2');
 
-//DB Params
-define('DB_HOST', 'your-host');
-define('DB_USER', 'your-username');
-define('DB_PASS', 'your-password');
-define('DB_NAME', 'your-db-name');
